2300f65666eea0cdab520cce3240306d93351bb2
hugoArg <ho.ochoa17@gmail.com>
2021-10-29 14:09:02 -0600

    BD edit

--- src/config/db.php
+++ src/config/db.php
@@ -1,9 +1,9 @@
 <?php
      class db{
-         private $dbHost ='localhost';
-         private $dbUser ='root';
-         private $dbPass ='';
-         private $dbname ='API';
+         private $dbHost ='us-cdbr-east-04.cleardb.com';
+         private $dbUser ='bc130be0438b5c';
+         private $dbPass ='056d9859';
+         private $dbname ='heroku_c10c0df37b6ef9c';
          //conexionysql
          public function conectDB(){
             $mysqlConnect = "mysql:host=$this->dbHost;dbname=$this->dbname";
